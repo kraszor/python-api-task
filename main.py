db387c409ea1e0e2e1918d92ea0d974278a3eb46
kraszor <kraszewski111@gmail.com>
2022-05-08 18:59:08 +0200

    use of Output class' methods in the main program loop

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ def main():
         if number < 5 or number > 20:
             print("The number is not between 5 and 20! Try again!")
             continue
-
-        # output code here
-
+        output = Output(number)
+        output.get_num_words()
+        print(f'\nYour {number} random words:\n')
+        output.display_random_words()
+        output.get_music_from_word()
+        print("The songs connected with your random words:\n")
+        print(output)
         decision = input("Press any key + Enter if you want to try again\n" +
                          "Press q + Enter if you want to exit\n")
         if decision == 'q':
